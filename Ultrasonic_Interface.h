e934db30fe0bc9356cfb79cbdf687ed18cd6fab6
sayed-Gomaa <62855362+sayed-Gomaa@users.noreply.github.com>
2021-07-10 04:50:52 -0700

    Add files via upload

---
+++ Ultrasonic_Interface.h
@@ -0,0 +1,15 @@
+/*
+ * IncFile1.h
+ *
+ * Created: 7/1/2021 2:05:41 PM
+ *  Author: Sayed Gomaa
+ */ 
+ #include "DIO/LIB/STD_TYPES.h"
+#ifndef INCFILE1_H_
+#define INCFILE1_H_
+
+
+ u16 ultarasonic_distance();
+
+
+#endif /* INCFILE1_H_ */
